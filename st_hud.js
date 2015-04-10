b53e7caf76b792be268e2c954d1f98c04536269e
Travis Clark <hiddenkrypt@gmail.com>
2015-04-10 16:30:30 -0400

    refactored ticker and sidebar into closures

--- st_hud.js
+++ st_hud.js
@@ -12,153 +12,159 @@ var st_hud = st_hud || function(){
 		initialize: function(){
 			ticker.load();
 			sidebar.load();
+			if( DEBUG ){ console.log( "st_hud initialized" ); }
 		}
 		,render: function( ctx ){
-			ticker.update();
+			ticker.publish();
 		}
 		,selectHexAtGrid: function( coords ){
 			if( st_data.loaded() ){
-				updateTickerData( st_data.getHexDataByGrid( coords ) );
+				ticker.update( st_data.getHexDataByGrid( coords ) );
 			}
 		}
-		,disableMouse: function(){ ticker.container.style["pointer-events"] = "none"; }
-		,enableMouse: function(){ ticker.container.style["pointer-events"] = "auto"; }
-	};
-	
-	var	updateTickerData = function ( hexData ){
-		var homeworld = st_data.getHomeworld();
-	
-		ticker.data = {
-			localCoordinates:{ 
-				x: -(homeworld.x - hexData.coords.x)
-				,y: homeworld.y - hexData.coords.y
-			}
-			,universalCoordinates:{
-				x: hashID.encode( hexData.coords.x + 100 )
-				,y: hashID.encode( hexData.coords.y + 100 )	
-			}
-			,territoryOwner: {r:0, g:0, b:0}
-			,systemName: "Unclaimed Space"
-		}		
-		if( hexData.system && hexData.system.name ){
-			ticker.data.systemName = hexData.system.name;
-		} else if( !hexData.owner && hexData.system ){
-			ticker.data.systemName = "Unclaimed System";
-		} else if( hexData.owner && !hexData.system ){
-			ticker.data.systemName = hexData.owner.adjective+" Space";
-		} 
-		if( hexData.owner && hexData.system ){
-			ticker.data.territoryOwner = hexData.owner;
+		,disableMouse: function(){ 
+		
+			ticker.disableMouse();
+			sidebar.disableMouse();
+		}
+		,enableMouse: function(){ 
+			ticker.enableMouse();
+			sidebar.enableMouse();
 		}
 	};
 	
-	var ticker = {
-		active: true
-		,container: {}
-		,title: {}
-		,owner: {}
-		,ucoords: {}
-		,lcoords: {}
-		,data: {
-			localCoordinates:{ x:-100, y:-100 }
-			,universalCoordinates:{ x:-100, y:-100 }
-			,territoryOwner: { r:0, g:0, b:0, name:"" }
-			,system: {}
-			,systemName: ""
-		}
-		,collapseIcon: {}
-		,toggle: function(){
-			if( this.active ){
-				for( var i = 0; i < this.container.childNodes.length; i++){
-					this.container.childNodes[i].style.display = 'none';
-				}
-				this.container.style.width = '1em';
-				this.collapseIcon.innerHTML = '>>';
-				this.collapseIcon.style.display = 'block';
-			} else{
-				for( var i = 0; i < this.container.childNodes.length; i++){
-					this.container.childNodes[i].style.display = 'block';
-				}
-				this.container.style.width = '13em';
-				this.collapseIcon.innerHTML = '<<';
-			}
-			this.active = !this.active;
-		}
-		,update: function(){
-			this.title.innerHTML = this.data.systemName;
-			this.owner.style.color = "rgb("+this.data.territoryOwner.r+","+this.data.territoryOwner.g+","+this.data.territoryOwner.b+")";
-			this.owner.innerHTML = this.data.territoryOwner.name || "&nbsp;";
-			this.lcoords.innerHTML = "HRC:["+this.data.localCoordinates.x + ", " + this.data.localCoordinates.y + "]";
-			this.ucoords.innerHTML = "UC: ["+this.data.universalCoordinates.x + ", " + this.data.universalCoordinates.y + "]";
-		}
-		,load: function(){
-			this.container = document.getElementById( 'hud_ticker_container' );
-			this.title = document.createElement( 'div' );
-			this.owner = document.createElement( 'div' );
-			this.lcoords = document.createElement( 'div' );
-			this.ucoords = document.createElement( 'div' );		
-			this.collapseIcon = document.createElement( 'div' );	
-			this.title.setAttribute( 'id', 'hud_ticker_title' );
-			this.owner.setAttribute( 'id', 'hud_ticker_owner' );
-			this.lcoords.setAttribute( 'id', 'hud_ticker_lcoords' );
-			this.ucoords.setAttribute( 'id', 'hud_ticker_ucoords' );
-			this.collapseIcon.setAttribute( 'id', 'hud_ticker_collapse_icon' ); 
-			this.title.setAttribute( 'title', 'System/Sector name' );
-			this.owner.setAttribute( 'title', 'System Owner' );
-			this.lcoords.setAttribute( 'title', 'Homeworld Relative Coordinates' );
-			this.ucoords.setAttribute( 'title', 'Universal Coordinates' );
-			this.collapseIcon.setAttribute( 'title', 'Hide / Show' );
-			this.collapseIcon.innerHTML = '<<';
-			this.collapseIcon.addEventListener('mousedown', function(e){ticker.toggle();});
-			this.container.appendChild( this.title );
-			this.container.appendChild( this.owner );
-			this.container.appendChild( this.lcoords );
-			this.container.appendChild( this.ucoords );
-			this.container.appendChild( this.collapseIcon );	
-			this.container.style.display = 'inline-block';
-		}
-	}; 
-	
-	var sidebar = {
-		active: true
-		,container: {}
-		,collapseIcon: {}
-		,hide: function(){}
-		,show: function(){}
-		,load: function(){
-			sidebar.container = document.getElementById( 'hud_sidebar_container' );
-			
-			sidebar.collapseIcon = document.createElement( 'div' );
-			
-			sidebar.collapseIcon.setAttribute( 'id', 'hud_sidebar_collapse_icon' ); 
-			
-			sidebar.collapseIcon.setAttribute( 'title', 'Hide / Show' );
-			
-			sidebar.collapseIcon.innerHTML = '>>';
-			
-			sidebar.collapseIcon.addEventListener('mousedown', function(e){sidebar.toggle();});
-			sidebar.container.appendChild( sidebar.collapseIcon );
-			
-			sidebar.container.style.display = 'inline-block';
-		}
-		,toggle: function(){
-			if( sidebar.active ){ //hide
-				for( var i = 0; i < sidebar.container.childNodes.length; i++){
-					sidebar.container.childNodes[i].style.display = 'none';
-				}
-				sidebar.container.style.width = '1em';
-				sidebar.collapseIcon.innerHTML = '<<';
-				sidebar.collapseIcon.style.display = 'block';
-			} else{ 
-				for( var i = 0; i < this.container.childNodes.length; i++){
-					this.container.childNodes[i].style.display = 'block';
-				}
-				sidebar.container.style.width = '15em';
-				sidebar.collapseIcon.innerHTML = '>>';
-			}
-			sidebar.active = !sidebar.active;
-		}
+	var createHudElement = function( tag, id, title, parent){
+		var element = document.createElement( tag );
+		element.setAttribute( 'id', id );
+		element.setAttribute( 'title', title );
+		parent.appendChild( element );
+		return element;
 	};
+	
+	var ticker = (function(){
+		var active = true
+			,container = {}
+			,title = {}
+			,owner = {}
+			,ucoords = {}
+			,lcoords = {}
+			,collapseIcon = {};
+		
+		return {
+			data: {
+				localCoordinates:{ x:-100, y:-100 }
+				,universalCoordinates:{ x:-100, y:-100 }
+				,territoryOwner: { r:0, g:0, b:0, name:"" }
+				,system: {}
+				,systemName: ""
+			}
+			,collapseIcon: {}
+			,toggle: function(){
+				if( active ){
+					for( var i = 0; i < container.childNodes.length; i++){
+						container.childNodes[i].style.display = 'none';
+					}
+					container.style.width = '1em';
+					collapseIcon.innerHTML = '>>';
+					collapseIcon.style.display = 'block';
+				} else{
+					for( var i = 0; i < container.childNodes.length; i++){
+						container.childNodes[i].style.display = 'block';
+					}
+					container.style.width = '13em';
+					collapseIcon.innerHTML = '<<';
+				}
+				active = !active;
+			}
+			,update: function( hexData ){
+				var homeworld = st_data.getHomeworld();
+				ticker.data = {
+					localCoordinates:{ 
+						x: -(homeworld.x - hexData.coords.x)
+						,y: homeworld.y - hexData.coords.y
+					}
+					,universalCoordinates:{
+						x: hashID.encode( hexData.coords.x + 100 )
+						,y: hashID.encode( hexData.coords.y + 100 )	
+					}
+					,territoryOwner: {r:0, g:0, b:0}
+					,systemName: "Unclaimed Space"
+				}		
+				if( hexData.system && hexData.system.name ){
+					ticker.data.systemName = hexData.system.name;
+				} else if( !hexData.owner && hexData.system ){
+					ticker.data.systemName = "Unclaimed System";
+				} else if( hexData.owner && !hexData.system ){
+					ticker.data.systemName = hexData.owner.adjective+" Space";
+				} 
+				if( hexData.owner && hexData.system ){
+					ticker.data.territoryOwner = hexData.owner;
+				}				
+			}
+			,publish: function(){
+				title.innerHTML = ticker.data.systemName;
+				owner.style.color = "rgb("+ticker.data.territoryOwner.r+","+ticker.data.territoryOwner.g+","+ticker.data.territoryOwner.b+")";
+				owner.innerHTML = ticker.data.territoryOwner.name || "&nbsp;";
+				lcoords.innerHTML = "HRC:["+ticker.data.localCoordinates.x + ", " + ticker.data.localCoordinates.y + "]";
+				ucoords.innerHTML = "UC: ["+ticker.data.universalCoordinates.x + ", " + ticker.data.universalCoordinates.y + "]";
+			}
+			,load: function(){
+				container = document.getElementById( 'hud_ticker_container' );
+				title = createHudElement( 'div', 'hud_ticker_title', 'System/Sector Name', container );
+				owner = createHudElement( 'div', 'hud_ticker_owner', 'System Owner', container );
+				lcoords = createHudElement( 'div', 'hud_ticker_lcoords', 'Homeworld Relative Coordinates', container );
+				ucoords = createHudElement( 'div', 'hud_ticker_ucoords', 'Universal Coordinates', container );
+				collapseIcon = createHudElement( 'div', 'hud_ticker_collapse_icon', 'Hide / Show', container );
+				collapseIcon.innerHTML = '<<';
+				collapseIcon.addEventListener('mousedown', function(e){ticker.toggle();});
+				container.style.display = 'inline-block';
+			}
+			,disableMouse: function(){
+				container.style["pointer-events"] = "none"; 	
+			}
+			,enableMouse: function(){
+				container.style["pointer-events"] = "auto"; 
+			}
+		};
+	})(); 
+	var sidebar = (function(){
+		var active = true
+			,container = {}
+			,collapseIcon = {};
+		return {
+			load: function(){
+				container = document.getElementById( 'hud_sidebar_container' );
+				collapseIcon = createHudElement( 'div', 'hud_sidebar_collapse_icon', 'Hide / Show', container );
+				collapseIcon.innerHTML = '>>';
+				collapseIcon.addEventListener( 'mousedown', function(e){ sidebar.toggle(); } );
+				container.style.display = 'inline-block';
+			}
+			,toggle: function(){
+				if( active ){ //hide
+					for( var i = 0; i < container.childNodes.length; i++){
+						container.childNodes[i].style.display = 'none';
+					}
+					container.style.width = '1em';
+					collapseIcon.innerHTML = '<<';
+					collapseIcon.style.display = 'block';
+				} else{ 
+					for( var i = 0; i < container.childNodes.length; i++){
+						container.childNodes[i].style.display = 'block';
+					}
+					container.style.width = '15em';
+					collapseIcon.innerHTML = '>>';
+				}
+				active = !active;
+			}
+			,disableMouse: function(){
+				container.style["pointer-events"] = "none"; 	
+			}
+			,enableMouse: function(){
+				container.style["pointer-events"] = "auto"; 
+			}
+		};
+	})();
+	
 	return hud;
 }(); // IIFE to create st_hud
 
